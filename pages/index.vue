4a9f6b6e7f748ee419c55b1158e0c170daa06863
Marina Moskaleva <moskaleva.m.a@yandex.ru>
2022-04-14 21:50:56 +0300

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <Tutorial />
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'IndexPage'
+})
+export default {
+  methods: {
+    test(){
+      console.log('test');
+    }
+  }
+}
+})
+</script>
