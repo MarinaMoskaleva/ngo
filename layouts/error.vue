f5cc0ae7d8c8a979977f39a4e756302f6560c390
Marina Moskaleva <moskaleva.m.a@yandex.ru>
2022-04-22 22:34:36 +0300

    error

---
+++ layouts/error.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="error-block">
+    <h1>О, нет! Ошибка :( Но не переживайте, у нас все под контролем!</h1>
+    <h2>Статус ошибки: {{error.statusCode}}</h2>
+    <h2>Сообщение: {{error.message}}</h2>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      error: {
+        type: Object,
+        default: () => ({})
+      }
+    }
+
+  }
+</script>
+
+<style scoped>
+.error-block{
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  height: 100vh;
+}
+
+</style>
